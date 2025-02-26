446d20350dae2527acb871988a916d1dae62be33
Seiko-And <seiko.a125@gmail.com>
2025-02-26 12:14:08 -0800

    added some comments

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,21 +1,22 @@
 
 // Promp user for a language code and then trim it and make it in lower case
 let lang = prompt('Choose your language code: es, de, en, fr');
-// Remove extra space and convert to lowercase if there is input
+// Only when there is input remove extra space and convert to lowercase
+// so if user hit cancel(Null) it still works
 if (lang){ 
     lang = lang.trim().toLowerCase(); 
 }
-// Put message in a variable
+// Initialize message in a variable 
 let message = '';
 
 // Determine the right language for the language code
-if (lang === 'es'){
+if (lang === 'es'){ // If the code is es
     message = 'Hello World translated in Spanish : Hola Mundo';
 }
-else if (lang === 'de'){
+else if (lang === 'de'){ // If the code is de
     message = 'Hello World translated in German : Hallo Welt';
 }
-else if (lang === 'fr'){
+else if (lang === 'fr'){  // If the code is fr
     message = 'Hello World translated in French : Bonjour le monde';
 }
 else{
