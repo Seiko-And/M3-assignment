b86d5b002c9b5a0088109e7e1e51f286522b73a6
Seiko-And <seiko.a125@gmail.com>
2025-02-26 12:37:57 -0800

    added some codes and comments

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -5,32 +5,38 @@ console.log(coinFlip); // Testing
 // Prompt user to enter Heads or Tails
 let choice = prompt('Enter Heads or Tails');
 
-//  Check if input is valid 
-if(choice ==='heads' || choice === 'tails'){
-   // If there is input, remove the exta space and convert input to lowercase
-   choice = choice.trim().toLowerCase();
-   console.log('user choose ' + choice); // Testing
+// If there is input, remove the extra space and convert input to lowercase
+if (choice){
+  choice = choice.trim().toLowerCase();
+  console.log('user choose ' + choice); // Testing
 
-   if (coinFlip < 1){// If the random number is 0
-      console.log('heads'); // Testing
-     
-      if (choice === 'heads'){  // If user choose Heads
-         alert('The flip was heads and you chose heads...you win!');
-      }
-      else if (choice === 'tails'){ //If user choose Tails
-         alert('The flip was heads but you chose tails...you lose!');
-      }
-   }
-   else {  // If the random number is 1
-      console.log('tail');
-      if (choice === 'heads'){  // If user choose Heads
-            alert('The flip was tails but you chose heads...you lose');
+   //  Check if input is valid 
+   if(choice ==='heads' || choice === 'tails'){
+      
+      if (coinFlip < 1){// If the random number is 0 (coinFlip === 0)
+         console.log('heads'); // Testing
+      
+         if (choice === 'heads'){  // If user choose Heads
+            alert('The flip was heads and you chose heads...you win!');
          }
-         else if (choice === 'tails'){  // If user choose Tails
-            alert('The flip was tails and you chose tails...you win!');
-         } 
+         else if (choice === 'tails'){ //If user choose Tails
+            alert('The flip was heads but you chose tails...you lose!');
+         }
+      }
+      else {  // If the random number is 1 (coinFlip === 1)
+         console.log('tails');
+         if (choice === 'heads'){  // If user choose Heads
+               alert('The flip was tails but you chose heads...you lose!');
+            }
+            else if (choice === 'tails'){  // If user choose Tails
+               alert('The flip was tails and you chose tails...you win!');
+            } 
+      }
    }
-}
+   else{  // If user enter invalid input
+      alert('Please enter Heads or Tails.');
+   }
+} 
 else{  // If user hit cancel or enter nothing
    alert('Please enter Heads or Tails.');
 }
