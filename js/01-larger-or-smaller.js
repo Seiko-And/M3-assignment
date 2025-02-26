604a76d5ed68e0e40b6a35882e00b355d1388f28
Seiko-And <seiko.a125@gmail.com>
2025-02-26 11:51:57 -0800

    added some comments

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,15 +1,17 @@
+// Prompt the usert to enter an integer
 let num1 = parseInt(prompt('Enter a first integer'));
 let num2 = parseInt(prompt('Enter a second integer'));
 
-if (isNaN(num1) || isNaN(num2)){
+// Check the larger number and display the result
+if (isNaN(num1) || isNaN(num2)){ // Check if inputs are not number 
     document.write('<p> Please enter valid numbers</p>')
 }
-else if (num1 > num2){
+else if (num1 > num2){ // If the first integer is larger
     document.write('<p> The larger number is ' + num1 + '</p>');
 }
-else if (num1 < num2) {
+else if (num1 < num2) {  // If the second integer is larger
     document.write('<p> The larger number is ' + num2 + '</p>');
 }
-else{
+else{ // If both numbers are equal
     document.write('<p> They are the same </p>');
 }
